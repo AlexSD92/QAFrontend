b75b5740f8e2d52336bb8abf96d49c2b7866e581
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-08-27 14:31:36 +0100

    QuestionDetails functional component created; useParams and useState hooks implemented. Slug passed through to URL for detail question view

---
+++ qafrontend/src/components/QuestionDetails.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {useState, useEffect} from 'react';
+import {useParams} from 'react-router-dom';
+
+// question-one-aa4ddf3d-288e-4efe-8be8-12f22c1718f9
+
+
+function QuestionDetails() {
+
+    const params = useParams()
+    const [questions, setQuestions] = useState([])
+
+    useEffect(() => {
+      fetch(`https://drf-api-master.herokuapp.com/questions/questions/${params.slug}`, {
+      method:'GET',
+      headers: {
+        'Content-Type':'application/json',
+        'Authorization':'Token 21eb499dc54e3b79dc02ebde1b9536346fb3ab40',
+      }
+      })
+      .then(resp => resp.json())
+      .then(result => {
+        setQuestions(result)
+      })
+      .catch(error => {
+        console.log(error)
+      }, [params.slug])
+    },)
+
+    return (
+        <div>
+                <div className='container m-5'>
+                <h2><a className='link-style'>{questions.question_summary}</a></h2>
+                <h6 className='badge rounded-pill bg-info'>{questions.question_author}</h6>
+                <h6 className='badge rounded-pill bg-secondary'>{questions.question_published}</h6>
+                <p>{questions.question_details}</p>
+                <p>{questions.slug}</p>
+                </div>
+        </div>
+        )
+}
+
+export default QuestionDetails;
